b6f09d14cb06bba7551512cd1f43b7b2818610cc
Chris Browder <rcbrowder@gmail.com>
2018-05-29 14:25:33 -0400

    Adds conversion functions and replaces innerHTML with converted tempuratures.

--- app/main.js
+++ app/main.js
@@ -1,14 +1,18 @@
 var weatherButton = document.getElementById('weatherButton');
-
-
-
+var results = [];
+// Output variables
+var cityOutput = document.getElementById('cityOutput');
+var tempK = document.getElementById('kel');
+var tempC = document.getElementById('cel');
+var tempF = document.getElementById('far');
 
 // Waits for page to load before firing
 document.onreadystatechange = function() {
     if (document.readyState == 'interactive') {
         // Initialize your application or run some code
-
         weatherButton.onclick = getWeather;
+
+
     }
 };
 
@@ -20,11 +24,21 @@ document.onreadystatechange = function() {
 
 
 function getWeather() {
-    alert('Test function ran');
+    cityOutput.innerHTML = results.name;
+    tempK.innerHTML = Math.round(results.main.temp) + ' K';
+    tempF.innerHTML = convertKtoF(results.main.temp) + ' F&deg';
+    tempC.innerHTML = convertKtoC(results.main.temp) + ' C&deg';
 }
 
+function convertKtoF (kelvin) {
+    var convertedFtemp = kelvin * (9/5) - 459.67;
+    return Math.round(convertedFtemp);
+}
 
-
+function convertKtoC(kelvin) {
+    var convertedCtemp = kelvin - 273.15;
+    return Math.round(convertKtoC);
+}
 
 
 
