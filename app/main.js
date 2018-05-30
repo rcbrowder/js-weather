24805e917d19dd0497f5b4a4bf25e0de4823d6e8
Chris Browder <rcbrowder@gmail.com>
2018-05-30 14:38:13 -0400

    Deleted example results

--- app/main.js
+++ app/main.js
@@ -7,50 +7,9 @@ var tempK = document.getElementById('temperatureOutputK');
 var tempF = document.getElementById('temperatureOutputF');
 var tempC = document.getElementById('temperatureOutputC');
 var condition = document.getElementById('condition');
+var weatherImage = document.getElementById('weatherImage');
 
-var results = {
-	coord: {
-		lon: -84.49,
-		lat: 38.02
-	},
-	weather: [
-		{
-			id: 801,
-			main: "Clouds",
-			description: "few clouds",
-			icon: "02d"
-		}
-	],
-	base: "stations",
-	main: {
-		temp: 301.17,
-		pressure: 1013,
-		humidity: 65,
-		temp_min: 300.15,
-		temp_max: 302.15
-	},
-	visibility: 16093,
-	wind: {
-		speed: 5.1,
-		deg: 90,
-		gust: 8.2
-	},
-	clouds: {
-		all: 20
-	},
-	dt: 1527614100,
-	sys: {
-		type: 1,
-		id: 1138,
-		message: 0.0038,
-		country: "US",
-		sunrise: 1527589076,
-		sunset: 1527641604
-	},
-	id: 420013316,
-	name: "Lexington",
-	cod: 200
-};
+
 
 // Waits for page to load before firing
 document.onreadystatechange = function() {
@@ -97,63 +56,3 @@ function convertKtoC(kelvin) {
 	var cel = kelvin - 273.15;
 	return Math.round(cel);
 }
-
-// URL pattern to follow is http://api.openweathermap.org/data/2.5/weather?zip=<zipCode>&us&appid=<myAPIKey>
-
-// If zip and API key are good, output looks like:
-// {
-// coord: {
-// lon: -84.49,
-// lat: 38.02
-// },
-// weather: [
-// {
-// id: 801,
-// main: "Clouds",
-// description: "few clouds",
-// icon: "02d"
-// }
-// ],
-// base: "stations",
-// main: {
-// temp: 301.17,
-// pressure: 1013,
-// humidity: 65,
-// temp_min: 300.15,
-// temp_max: 302.15
-// },
-// visibility: 16093,
-// wind: {
-// speed: 5.1,
-// deg: 90,
-// gust: 8.2
-// },
-// clouds: {
-// all: 20
-// },
-// dt: 1527614100,
-// sys: {
-// type: 1,
-// id: 1138,
-// message: 0.0038,
-// country: "US",
-// sunrise: 1527589076,
-// sunset: 1527641604
-// },
-// id: 420013316,
-// name: "Lexington",
-// cod: 200
-// }
-
-// If zip is bad, output looks like:
-// {
-// cod: "404",
-// message: "city not found"
-// }
-
-
-// If my API key is bad or missing, output looks like:
-// {
-// cod: 401,
-// message: "Invalid API key. Please see http://openweathermap.org/faq#error401 for more info."
-// }
