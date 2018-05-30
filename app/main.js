58b8396880180ababf250f58ba6f833a05e0d1a4
Chris Browder <rcbrowder@gmail.com>
2018-05-30 09:14:15 -0400

    Adds tempurature conversions

--- app/main.js
+++ app/main.js
@@ -1,7 +1,10 @@
 var weatherButton = document.getElementById('weatherButton');
 var results = [];
 // Output variables
+var output = document.getElementById('output');
 var cityOutput = document.getElementById('cityOutput');
+var conditions = document.getElementById('conditions');
+
 var tempK = document.getElementById('kel');
 var tempC = document.getElementById('cel');
 var tempF = document.getElementById('far');
@@ -18,16 +21,25 @@ document.onreadystatechange = function() {
 
 
 
-
-
-
-
-
 function getWeather() {
+
+    // Code that fetches data from API URL and stores it in results.
+
+    // if results.code == 200
+
     cityOutput.innerHTML = results.name;
     tempK.innerHTML = Math.round(results.main.temp) + ' K';
     tempF.innerHTML = convertKtoF(results.main.temp) + ' F&deg';
     tempC.innerHTML = convertKtoC(results.main.temp) + ' C&deg';
+    conditions.innerHTML = results.weather[0].description;
+
+    output.style.display = 'block';
+
+    // else if cod == 404
+
+    // else if cod == 401
+
+    //else
 }
 
 function convertKtoF (kelvin) {
@@ -43,18 +55,9 @@ function convertKtoC(kelvin) {
 
 
 
-
-
-
-
-
-
-
-
-
 // URL pattern to follow is http://api.openweathermap.org/data/2.5/forecast?id=blahblahblah
 
-// if zip is bad output looks like:
+// If zip is bad output looks like:
 // {
 // cod: "404",
 // message: "city not found"
